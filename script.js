5a512458da94d674cbb691f6bc91b8b58de6a512
Omkar Patange <omkarpatange2002@gmail.com>
2019-09-11 17:44:50 +0530

    d
    d

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var input = Number(prompt("Put in a number"));
+alert("Enter values from 0");
+
+ 	var input = Number(prompt("Put in a number"));
  	var a = "Decimal : "+ input+"<br>";
  	var b = "Binary : "+ input.toString(2)+"<br>";
 	var c = "Octal : "+ input.toString(8)+"<br>";
@@ -8,3 +10,16 @@ var input = Number(prompt("Put in a number"));
 	document.getElementById("showvalue2").innerHTML = b;
 	document.getElementById("showvalue3").innerHTML = c;
 	document.getElementById("showvalue4").innerHTML = d;
+
+	function again(){
+		var input = Number(prompt("Put in a number"));
+ 	var a = "Decimal : "+ input+"<br>";
+ 	var b = "Binary : "+ input.toString(2)+"<br>";
+	var c = "Octal : "+ input.toString(8)+"<br>";
+	var d = "HexaDecimal : "+ input.toString(16)+"<br>";
+
+	document.getElementById("showvalue1").innerHTML = a;
+	document.getElementById("showvalue2").innerHTML = b;
+	document.getElementById("showvalue3").innerHTML = c;
+	document.getElementById("showvalue4").innerHTML = d;
+	}
